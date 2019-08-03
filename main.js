825d2020fd42bd754020ee60741d654fbd6fa942
ahmedzafar0 <53649796+ahmedzafar0@users.noreply.github.com>
2019-08-03 19:26:05 +0500

    test-website

---
+++ main.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBW-z6_VvK_OxsLMgwLM7K0v9YuoRg8nGk",
+    authDomain: "tution-academy-f22ff.firebaseapp.com",
+    databaseURL: "https://tution-academy-f22ff.firebaseio.com",
+    projectId: "tution-academy-f22ff",
+    storageBucket: "",
+    messagingSenderId: "636458474600",
+    appId: "1:636458474600:web:2d59558d557bf9bc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
